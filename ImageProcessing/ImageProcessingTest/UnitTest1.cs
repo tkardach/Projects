d2d38d4a6a4b22b7179db2fecd3f49f684f49817
Thomas Kardach <tommy@kardach.com>
2019-09-10 11:16:35 -0700

    Update unit test

--- ImageProcessing/ImageProcessingTest/UnitTest1.cs
+++ ImageProcessing/ImageProcessingTest/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using ImageProcessing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing.Imaging;
@@ -24,8 +24,7 @@ namespace ImageProcessingTest
             var image = ImageProcessing.Compressor.RemoveExif(@"Resources/ExifTestPhoto.jpeg");
             image.Save(@"Resources/PostExifRemove.jpg", ImageFormat.Jpeg);
 
-            Assert.IsTrue(new FileInfo(@"Resources/ExifTestPhoto.jpeg").Length > new FileInfo(@"Resources/PostExifRemove.jpg").Length);
-        }
+            Assert.IsTrue(new FileInfo(@"Resources/ExifTestPhoto.jpeg").Length > new FileInfo(@"Resources/PostExifRemove.jpg").Length);        }
 
         [TestMethod]
         public void TestResize()
@@ -38,6 +37,20 @@ namespace ImageProcessingTest
 
                 Assert.IsTrue(longest == 512);
             }
+        }
+
+        [TestMethod]
+        public void TestThumbnailCreation()
+        {
+            var image = ImageProcessing.Compressor.RemoveExif(@"Resources/ExifTestPhoto90.jpeg");
+            image.Save(@"Resources/PostExifRemove.jpg", ImageFormat.Jpeg);
+            
+            Bitmap bmp = new Bitmap(@"Resources/PostExifRemove.jpg");
+            bmp = ImageProcessing.Compressor.ResizeImage(bmp, 512);
+            bmp.Save(@"Resources/ThumbnailResized.jpg", bmp.RawFormat);
+            
+            ImageProcessing.Compressor.CompressImage(@"Resources/ThumbnailResized.jpg", @"Resources/ThumbnailResizedCompressed.jpg", 85, ImageFormat.Jpeg);
+            Assert.IsTrue(new FileInfo(@"Resources/ThumbnailResized.jpg").Length > new FileInfo(@"Resources/ThumbnailResizedCompressed.jpg").Length);
         }
     }
 }
